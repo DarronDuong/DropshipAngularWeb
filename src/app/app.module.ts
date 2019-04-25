01780ec319d91d24a256c170fd427221d5a996f2
TrungDuong <tuantrung1512@outlook.com>
2019-04-25 15:03:38 +1000

    add product-form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,6 +23,7 @@ import { AuthService } from './auth.service';
 import { AuthGuard } from './auth-guard.service';
 import { UserService } from './user.service';
 import { AdminAuthGuard } from './admin-auth-guard.service';
+import { ProductFormComponent } from './admin/product-form/product-form.component';
 
 @NgModule({
   declarations: [
@@ -36,7 +37,8 @@ import { AdminAuthGuard } from './admin-auth-guard.service';
     MyOrderComponent,
     AdminProductsComponent,
     AdminOrdersComponent,
-    LoginComponent
+    LoginComponent,
+    ProductFormComponent
   ],
   imports: [
     BrowserModule,
@@ -56,6 +58,8 @@ import { AdminAuthGuard } from './admin-auth-guard.service';
       { path: 'my/orders', component : MyOrderComponent, canActivate: [AuthGuard]},
       
       { path: 'admin/products', component : AdminProductsComponent, canActivate: [AuthGuard, AdminAuthGuard]},
+      { path: 'admin/products/new', component : ProductFormComponent, canActivate: [AuthGuard, AdminAuthGuard]},
+
       { path: 'admin/orders', component : AdminOrdersComponent, canActivate: [AuthGuard, AdminAuthGuard]},
     ])
   ],
