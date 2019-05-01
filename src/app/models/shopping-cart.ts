9a7b70b15cd2572155aed6c095c2ad6c850027be
Trung Duong <trungd@accelrelation.com.au>
2019-05-02 08:43:39 +1000

    modify shopping-cart to display price

--- src/app/models/shopping-cart.ts
+++ src/app/models/shopping-cart.ts
@@ -1,13 +1,15 @@
 import { ShoppingCartItem } from './shopping-cart-item';
+import { TouchSequence } from 'selenium-webdriver';
 
 export class ShoppingCart {
 
     itemList: ShoppingCartItem[] = [];
 
     constructor(public items: { [key: string]: ShoppingCartItem }) {
-        debugger;
-        for (let productId in items)
-            this.itemList.push(items[productId]);
+        for (let productId in items){
+            let item = items[productId];
+            this.itemList.push(new ShoppingCartItem(item.product, item.quantity));
+        }
     }
 
     get totalItemsCount(){
@@ -16,6 +18,14 @@ export class ShoppingCart {
             count += this.items[productId].quantity;
         }
         return count;
+    }
+
+    get totalPrice() {
+        let sum = 0;
+        for (let productId in this.items)
+            sum += this.items[productId].totalPrice;
+
+        return sum;
     }
 
 }
