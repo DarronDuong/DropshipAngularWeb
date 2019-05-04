7f62e91ae9c15efd8db69a55553d4ec1968cb690
TrungDuong <tuantrung1512@outlook.com>
2019-05-04 22:13:42 +1000

    add icon, fix date time format

--- src/app/shared/services/order.service.ts
+++ src/app/shared/services/order.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from 'angularfire2/database';
 import { ShoppingCartService } from 'shared/services/shopping-cart.service';
+import { Order } from 'shared/models/order';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,7 @@ export class OrderService {
   constructor(private db: AngularFireDatabase,
               private shoppingCartService: ShoppingCartService) { }
 
-  async placeOrder(order){
+  async placeOrder(order: Order){
     let result = await this.db.list('/orders').push(order);
     this.shoppingCartService.clearCart();
     return result;
