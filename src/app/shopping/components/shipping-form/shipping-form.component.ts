c3eb5386592840ef07f5930347dc18644aaedb42
TrungDuong <tuantrung1512@outlook.com>
2019-05-04 23:23:57 +1000

    fix bug shipping type when build production

--- src/app/shopping/components/shipping-form/shipping-form.component.ts
+++ src/app/shopping/components/shipping-form/shipping-form.component.ts
@@ -12,7 +12,7 @@ import { ShoppingCart } from 'shared/models/shopping-cart';
   styleUrls: ['./shipping-form.component.css']
 })
 export class ShippingFormComponent implements OnInit, OnDestroy {
-  shipping = {}; 
+  shipping: any; 
   subscription: Subscription;
   userId: string;
   @Input('cart') cart : ShoppingCart;
